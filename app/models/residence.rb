92762cf2b23d2889a2e72e4d08bc778d5c4044da
[tseferi] <[teala.seferi@cns.uist.edu.mk]>
2019-11-06 13:41:32 +0100

    Deleted code that was commented

--- app/models/residence.rb
+++ app/models/residence.rb
@@ -1,2 +1,23 @@
 class Residence < ApplicationRecord
+    belongs_to :category, touch: true
+  after_touch do
+    puts 'A residence was touched'
+  end
+  
+  validate :word_count_is_more_than_25
+
+  validates :title, length: {is: 5}
+  validates :available, inclusion: [true, false]
+  validates :unit, length: {is: 4}
+  validates :rent, length: {is: 6 }
+
+  before_save :capitalize_title
+  def capitalize_title 
+    self.title.capitalize!
+  end
+
+  def word_count_is_more_than_25
+    errors.add(:residence, "Error") if description.split(" ").size < 25
+  end
+
 end
